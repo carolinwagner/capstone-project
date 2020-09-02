57dcef28c16260bea838db0a37cb2b8473be5273
Carolin Wagner <ich.caro@gmail.com>
2020-09-02 22:40:57 +0200

    Add paragraphs in Header

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,19 @@ import React from 'react'
 import styled from 'styled-components/macro'
 
 export default function Header() {
-  return <StyledHeader>Hello Bylaws</StyledHeader>
+  return (
+    <>
+      <StyledHeader>Hello Bylaws</StyledHeader>
+      <p>
+        Jeder gemeinnützige Verein benötigt gemäß § 57 des Bürgerlichen
+        Gesetzbuches eine Satzung.
+      </p>
+      <p>
+        Beantworte die folgenden Fragen und lass dir eine Satzung, die
+        individuell auf deinen Verein abgestimmt ist, generieren.
+      </p>
+    </>
+  )
 }
 
 const StyledHeader = styled.h1`
