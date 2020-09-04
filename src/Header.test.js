207e30cc206fe1909ef7d2cae09bfa7c542a24a9
Carolin Wagner <ich.caro@gmail.com>
2020-09-04 18:57:12 +0200

    Add Header snapshot test

--- src/Header.test.js
+++ src/Header.test.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import renderer from 'react-test-renderer'
 import Header from './Header'
 
 describe('Header', () => {
   it('shows the main headline', () => {
     const { getByText } = render(<Header />)
     expect(getByText('Hello Bylaws')).toBeInTheDocument()
+  })
+})
+
+describe('Header', () => {
+  it('renders Header correctly', () => {
+    const tree = renderer.create(<Header />)
+    expect(tree).toMatchSnapshot()
   })
 })
