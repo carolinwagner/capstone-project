8d5544b01976508d31558f319fe78face2470ea9
Carolin Wagner <ich.caro@gmail.com>
2020-09-10 17:55:10 +0200

    Add input cypress tests and start all-answers-summary test

--- cypress/integration/QuestionForm.spec.js
+++ cypress/integration/QuestionForm.spec.js
@@ -2,7 +2,6 @@
 
 context('QuestionForm', () => {
   beforeEach(() => {
-    cy.log('hallo')
     cy.visit('/')
   })
 
@@ -15,14 +14,52 @@ context('QuestionForm', () => {
   })
 
   it('has a button', () => {
-    cy.get('button').contains('Zusammenfassung anzeigen')
+    cy.get('button').contains('Zusammenfassung anzeigen').click()
   })
 
-  it('has questions', () => {
+  it('has more than 20 questions', () => {
     cy.get('h2').should('have.length.gt', 20)
   })
 
   it('has some inputs', () => {
-    cy.get('input').should('have.length', 54)
+    cy.get('input').should('have.length.gt', 20)
+  })
+
+  it('shows text input in question no. 1', () => {
+    cy.get('label')
+      .contains('Name des Vereins')
+      .get('input[id=clubName]')
+      .type('Testverein')
+  })
+
+  it('shows text input in question no. 2', () => {
+    cy.get('label')
+      .contains('Stadt')
+      .get('input[id=clubLocation]')
+      .type('Teststadt')
+      .should('have.value', 'Teststadt')
+  })
+
+  it('shows number input in question no. 9', () => {
+    cy.get('label')
+      .contains('Wie viele Tage vorher soll zur Mitgliederversammlung')
+      .get('input[id=generalMeetingDeadline]')
+      .type('15')
+      .parents()
+      .get('button')
+      .click()
+  })
+
+  it.only('shows all answers in the summary', () => {
+    cy.get('input[id=1]')
+      .type('Cypress Testverein')
+      .get('input[id=2]')
+      .type('Hamburg')
+      .get('input[id=3]')
+      .type('testen')
+      .get('input[id=inner1]')
+      .click()
+      .get('input[id=inner2]')
+      .click()
   })
 })
